53ec275f1a4e7a91cdca0d2d1c84698f9fb44db7
Paul Ryan <paulwryan97@gmail.com>
2021-01-03 19:26:40 +0000

    Remove sample app files

--- electron-sample-app/testRenderer.js
+++
@@ -1,9 +0,0 @@
-const childIpc = require('electron-child-ipc');
-
-childIpc.sendToChildProcess('child1', {id: 'test', args: []}).then((result) => {
-    console.log(result);
-});
-
-childIpc.sendToChildProcess('child1', {id: 'test2', args: []}).then((result) => {
-    console.log(result);
-});
